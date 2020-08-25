d0a438dab9402774298c992c97ec321b893f6760
sundeep <sundeep1111-code@gmail.com>
2020-08-25 16:48:27 +0530

    Updates

---
+++ precache-manifest.03fba3a1b33a1bb3c54e2c513b78970a.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e70367a476c827f5c4de166524008428",
+    "url": "/foodapp/index.html"
+  },
+  {
+    "revision": "edbb7750cef0ceab113e",
+    "url": "/foodapp/static/css/main.28f19dd9.chunk.css"
+  },
+  {
+    "revision": "f918d5abf84e8e0b4d6f",
+    "url": "/foodapp/static/js/2.9b93d48f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/foodapp/static/js/2.9b93d48f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edbb7750cef0ceab113e",
+    "url": "/foodapp/static/js/main.1a811946.chunk.js"
+  },
+  {
+    "revision": "250028cafa53ec4f388b",
+    "url": "/foodapp/static/js/runtime-main.7ab1a8c0.js"
+  },
+  {
+    "revision": "f1cde2e5ec239119c0a30ba20ed50be5",
+    "url": "/foodapp/static/media/img-all-food.f1cde2e5.png"
+  },
+  {
+    "revision": "c6b335b65ca418f215d856286a43017c",
+    "url": "/foodapp/static/media/img-arr-down.c6b335b6.png"
+  },
+  {
+    "revision": "e7db60efddad36de144745e3e694e822",
+    "url": "/foodapp/static/media/img-arr-right.e7db60ef.png"
+  },
+  {
+    "revision": "50e9c91d9170f84b3ba4c65600dfa6ec",
+    "url": "/foodapp/static/media/img-arrow-right.50e9c91d.png"
+  },
+  {
+    "revision": "2c51666dd501765c29992bbc9289de64",
+    "url": "/foodapp/static/media/img-asians.2c51666d.png"
+  },
+  {
+    "revision": "486a7c5578583ecf60df48faf23d0538",
+    "url": "/foodapp/static/media/img-barbeque.486a7c55.png"
+  },
+  {
+    "revision": "5e0c08cd23a99f3ff8681f00c9077d13",
+    "url": "/foodapp/static/media/img-burger.5e0c08cd.png"
+  },
+  {
+    "revision": "115224dc98b7168aa4e35c46d1aff204",
+    "url": "/foodapp/static/media/img-burgers.115224dc.png"
+  },
+  {
+    "revision": "01787237b5d25e26cdeea979e10f1c3f",
+    "url": "/foodapp/static/media/img-celeb.01787237.png"
+  },
+  {
+    "revision": "cb4ab16334cfcac6c18d4c18c0aef337",
+    "url": "/foodapp/static/media/img-clock.cb4ab163.png"
+  },
+  {
+    "revision": "671a65a8a5fc885b0b97c4c9c0b4d599",
+    "url": "/foodapp/static/media/img-dessert.671a65a8.png"
+  },
+  {
+    "revision": "97636784c3fd64dab3d22aade317ce82",
+    "url": "/foodapp/static/media/img-menu.97636784.png"
+  },
+  {
+    "revision": "410e248483e37367a500aeb0b3e25228",
+    "url": "/foodapp/static/media/img-pizza.410e2484.png"
+  },
+  {
+    "revision": "352b0441cbb82becdd2ef1e265b37e4f",
+    "url": "/foodapp/static/media/img-search.352b0441.png"
+  },
+  {
+    "revision": "d81a4aca5501ededa65411dac5a0ea3d",
+    "url": "/foodapp/static/media/img-smiley.d81a4aca.png"
+  },
+  {
+    "revision": "a4d24a7053eb6f30528ca48e7433d820",
+    "url": "/foodapp/static/media/img-star.a4d24a70.png"
+  },
+  {
+    "revision": "ec9a75df40f0908d8d742da8794731f8",
+    "url": "/foodapp/static/media/img-sushi.ec9a75df.png"
+  },
+  {
+    "revision": "671884223baaa215ef3f05efdf5ba71a",
+    "url": "/foodapp/static/media/img-thai.67188422.png"
+  },
+  {
+    "revision": "7afe97e679fa5e82ddcfe1f0fcdd48d0",
+    "url": "/foodapp/static/media/img-user.7afe97e6.png"
+  },
+  {
+    "revision": "e0d7608ecbdbd11bd0d7ebd83a2024df",
+    "url": "/foodapp/static/media/landing-illustration.e0d7608e.png"
+  }
+]);
